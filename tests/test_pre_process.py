39e817632afda94bf0a816eb024e8ce279af42fb
hemengyang <hmy0119@gmail.com>
2022-01-30 14:43:38 +0800

    test: 添加去除字符的测试

---
+++ tests/test_pre_process.py
@@ -0,0 +1,24 @@
+import pytest
+from nonebug import App
+
+
+@pytest.mark.asyncio
+async def test_remove_emoji(app: App):
+    """测试移除 emoji"""
+
+    from nonebot_plugin_wordcloud.data import pre_precess
+
+    msg = "1😅2"
+    msg = pre_precess(msg)
+    assert msg == "12"
+
+
+@pytest.mark.asyncio
+async def test_remove_http(app: App):
+    """测试移除网址"""
+
+    from nonebot_plugin_wordcloud.data import pre_precess
+
+    msg = "1 https://v2.nonebot.dev/ 2"
+    msg = pre_precess(msg)
+    assert msg == "1  2"
