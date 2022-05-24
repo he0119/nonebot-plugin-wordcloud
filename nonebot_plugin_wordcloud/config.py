7eac0d99444b3614adf8571dc06832352444c40c
hemengyang <hmy0119@gmail.com>
2022-05-25 00:00:16 +0800

    使用结巴分词提供的关键词抽取，并删除自带的停用词表

--- nonebot_plugin_wordcloud/config.py
+++ nonebot_plugin_wordcloud/config.py
@@ -10,7 +10,7 @@ class Config(BaseModel, extra=Extra.ignore):
     wordcloud_height: int = 1200
     wordcloud_background_color: str = "black"
     wordcloud_font_path: str = str(Path(__file__).parent / "SourceHanSans.otf")
-    wordcloud_stopwords_path: Path = Path(__file__).parent / "stopwords.txt"
+    wordcloud_stopwords_path: Optional[Path]
     wordcloud_userdict_path: Optional[Path]
     wordcloud_timezone: Optional[str]
 
