c7d5c9f93ffd0f10a8a342e3c7f5f487aa1282c7
hemengyang <hmy0119@gmail.com>
2022-06-30 08:40:12 +0800

    更换匹配网址的正则表达式，修复无法匹配微博分享网址的问题

--- tests/test_pre_process.py
+++ tests/test_pre_process.py
@@ -19,6 +19,14 @@ async def test_remove_http(app: App):
 
     from nonebot_plugin_wordcloud.data_source import pre_precess
 
+    msg = "1 http://v2.nonebot.dev/ 2"
+    msg = pre_precess(msg)
+    assert msg == "1  2"
+
     msg = "1 https://v2.nonebot.dev/ 2"
+    msg = pre_precess(msg)
+    assert msg == "1  2"
+
+    msg = "1 https://share.api.weibo.cn/share/312975272,4779675790873388.html?weibo_id=4779675790873388 2"
     msg = pre_precess(msg)
     assert msg == "1  2"
