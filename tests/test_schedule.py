d924b8ffe237b7e115af1849a0666c6c853760ad
hemengyang <hmy0119@gmail.com>
2022-12-26 00:02:48 +0800

    添加每日定时发送词云的测试

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -1,6 +1,7 @@
 from datetime import time
 
 from nonebug import App
+from pytest_mock import MockerFixture
 
 from .utils import fake_group_message_event
 
@@ -75,3 +76,46 @@ async def test_disable_schedule(app: App):
         assert len(results.all()) == 0
 
     assert len(schedule_service.schedules) == 2
+
+
+async def test_run_task(app: App, mocker: MockerFixture):
+    from nonebot.adapters.onebot.v11 import MessageSegment
+    from nonebot_plugin_datastore import create_session
+
+    from nonebot_plugin_wordcloud import schedule_service
+    from nonebot_plugin_wordcloud.model import Schedule
+
+    async with create_session() as session:
+        schedule = Schedule(bot_id="test", group_id="10000")
+        session.add(schedule)
+        await session.commit()
+
+    mocked_get_message_records = mocker.patch(
+        "nonebot_plugin_wordcloud.schedule.get_message_records", return_value=["test"]
+    )
+    mocked_bot = mocker.AsyncMock()
+    mocked_get_bot = mocker.patch(
+        "nonebot_plugin_wordcloud.schedule.get_bot", return_value=mocked_bot
+    )
+    mocked_get_wordcloud = mocker.patch(
+        "nonebot_plugin_wordcloud.schedule.get_wordcloud", return_value="test"
+    )
+
+    await schedule_service.run_task()
+
+    mocked_get_bot.assert_called_once_with("test")
+    mocked_get_message_records.assert_called_once()
+    mocked_get_wordcloud.assert_called_once_with(["test"])
+    mocked_bot.send_group_msg.assert_called_once_with(
+        group_id="10000",
+        message=MessageSegment(
+            type="image",
+            data={
+                "file": "test",
+                "type": None,
+                "cache": "true",
+                "proxy": "true",
+                "timeout": None,
+            },
+        ),
+    )
