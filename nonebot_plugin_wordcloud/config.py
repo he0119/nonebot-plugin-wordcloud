d8a94b80ad90f6a9ec57123e2ef68f78b481c7ba
hemengyang <hmy0119@gmail.com>
2022-12-26 00:24:52 +0800

    修复配置未能正确加载的问题

--- nonebot_plugin_wordcloud/config.py
+++ nonebot_plugin_wordcloud/config.py
@@ -28,22 +28,30 @@ class Config(BaseModel, extra=Extra.ignore):
     wordcloud_default_schedule_time: time
     """ 默认定时发送时间
 
-    如果群内不单独设置则使用这个值，默认为晚上 10 点
+    如果群内不单独设置则使用这个值，默认为晚上 10 点，时区为设定的时区
     """
 
     @root_validator(pre=True, allow_reuse=True)
     def set_default_values(cls, values):
-        if "wordcloud_font_path" not in values:
+        if not values.get("wordcloud_font_path"):
             values["wordcloud_font_path"] = str(
                 Path(__file__).parent / "SourceHanSans.otf"
             )
-        if "wordcloud_timezone" in values:
-            default_schedule_time = values["wordcloud_default_schedule_time"].replace(
-                tzinfo=ZoneInfo(values["wordcloud_timezone"])
+
+        if wordcloud_default_schedule_time := values.get(
+            "wordcloud_default_schedule_time"
+        ):
+            default_schedule_time = time.fromisoformat(wordcloud_default_schedule_time)
+        else:
+            default_schedule_time = time(22, 0, 0)
+
+        if wordcloud_timezone := values.get("wordcloud_timezone"):
+            default_schedule_time = default_schedule_time.replace(
+                tzinfo=ZoneInfo(wordcloud_timezone)
             )
         else:
-            default_schedule_time = time(
-                22, 0, 0, tzinfo=datetime.now().astimezone().tzinfo
+            default_schedule_time = default_schedule_time.replace(
+                tzinfo=datetime.now().astimezone().tzinfo
             )
         values["wordcloud_default_schedule_time"] = default_schedule_time
         return values
