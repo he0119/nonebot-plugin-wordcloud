c597b172cb6682a67d3e4142e6247ecb641a386a
hemengyang <hmy0119@gmail.com>
2022-12-26 20:32:25 +0800

    稍微修改了一下时区逻辑，完善测试

--- nonebot_plugin_wordcloud/utils.py
+++ nonebot_plugin_wordcloud/utils.py
@@ -22,9 +22,11 @@ def get_datetime_now_with_timezone() -> datetime:
 def get_datetime_fromisoformat_with_timezone(date_string: str) -> datetime:
     """从 iso8601 格式字符串中获取时间，并包含时区信息"""
     if plugin_config.wordcloud_timezone:
-        return datetime.fromisoformat(date_string).astimezone(
-            ZoneInfo(plugin_config.wordcloud_timezone)
-        )
+        raw = datetime.fromisoformat(date_string)
+        if raw.tzinfo:
+            return raw.astimezone(ZoneInfo(plugin_config.wordcloud_timezone))
+        else:
+            return raw.replace(tzinfo=ZoneInfo(plugin_config.wordcloud_timezone))
     else:
         return datetime.fromisoformat(date_string).astimezone()
 
@@ -41,9 +43,11 @@ def time_astimezone(time: time, tz: Optional[ZoneInfo] = None) -> time:
 def get_time_fromisoformat_with_timezone(time_string: str) -> time:
     """从 iso8601 格式字符串中获取时间，并包含时区信息"""
     if plugin_config.wordcloud_timezone:
-        return time_astimezone(
-            time.fromisoformat(time_string), ZoneInfo(plugin_config.wordcloud_timezone)
-        )
+        raw = time.fromisoformat(time_string)
+        if raw.tzinfo:
+            return time_astimezone(raw, ZoneInfo(plugin_config.wordcloud_timezone))
+        else:
+            return raw.replace(tzinfo=ZoneInfo(plugin_config.wordcloud_timezone))
     else:
         return time_astimezone(time.fromisoformat(time_string))
 
--- tests/test_timezone.py
+++ tests/test_timezone.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, time
 
 try:
     from zoneinfo import ZoneInfo
@@ -64,3 +64,37 @@ async def test_set_timezone(app: App, mocker: MockerFixture):
     mocked_datetime.now.assert_called_once_with(ZoneInfo("UTC"))
     mocked_datetime.fromisoformat.assert_any_call("2022-01-01")
     mocked_datetime.fromisoformat().astimezone.assert_called_once_with(ZoneInfo("UTC"))
+
+
+async def test_time_astimezone(app: App):
+    from nonebot_plugin_wordcloud.utils import time_astimezone
+
+    assert time_astimezone(time(10, 0, 0)).isoformat() == "10:00:00+08:00"
+
+    assert (
+        time_astimezone(time(10, 0, 0, tzinfo=ZoneInfo("UTC"))).isoformat()
+        == "18:00:00+08:00"
+    )
+
+
+async def test_get_time(app: App):
+    from nonebot_plugin_wordcloud.config import plugin_config
+    from nonebot_plugin_wordcloud.utils import get_time_fromisoformat_with_timezone
+
+    assert (
+        get_time_fromisoformat_with_timezone("10:00:00").isoformat() == "10:00:00+08:00"
+    )
+    assert (
+        get_time_fromisoformat_with_timezone("10:00:00+07:00").isoformat()
+        == "11:00:00+08:00"
+    )
+
+    plugin_config.wordcloud_timezone = "UTC"
+
+    assert (
+        get_time_fromisoformat_with_timezone("10:00:00").isoformat() == "10:00:00+00:00"
+    )
+    assert (
+        get_time_fromisoformat_with_timezone("10:00:00+08:00").isoformat()
+        == "02:00:00+00:00"
+    )
