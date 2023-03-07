0fdd57e936f7cf3d25d6401f2ae3c40245a2a364
hemengyang <hmy0119@gmail.com>
2023-03-07 11:09:40 +0800

    ci: 添加 PostgreSQL 与 MySQL 的测试

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,12 +3,13 @@ from pathlib import Path
 import nonebot
 import pytest
 from nonebug import NONEBOT_INIT_KWARGS, App
-from sqlalchemy import delete
+from sqlalchemy import StaticPool, delete
 
 
 def pytest_configure(config: pytest.Config) -> None:
     config.stash[NONEBOT_INIT_KWARGS] = {
-        "datastore_database_url": "sqlite+aiosqlite:///:memory:",
+        "datastore_database_url": "sqlite+aiosqlite://",
+        "datastore_engine_options": {"poolclass": StaticPool},
     }
 
 
