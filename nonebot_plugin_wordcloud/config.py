2c11f237bbb3f7d85e029ecf809a7f8917d0e687
uy/sun <hmy0119@gmail.com>
2025-04-27 08:41:44 +0800

    feat: 添加是否发送词云图片时回复消息的配置项

--- nonebot_plugin_wordcloud/config.py
+++ nonebot_plugin_wordcloud/config.py
@@ -29,6 +29,8 @@ class Config(BaseModel):
     wordcloud_options: dict[str, Any] = {}
     wordcloud_exclude_user_ids: set[str] = set()
     """排除的用户 ID 列表（全局，不区分平台）"""
+    wordcloud_reply_message: bool = False
+    """是否回复消息，默认不回复"""
 
     @model_validator(mode="before")
     def set_default_values(cls, values):
